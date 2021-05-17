7c24a63a60cf6ef955f9c6af1be5ea7eae663985
Andre Alves <andre.alves@ccc.ufcg.edu.br>
2021-05-17 19:16:28 -0300

    Adicionada a função de menu e alteração dos comentários de algumas seções

--- main.py
+++ main.py
@@ -1,43 +1,101 @@
 import pygame
+from time import sleep
 
-#importa uma classe para importar sons e tocar a musica
+from pygame.constants import MOUSEBUTTONDOWN
+
+# Importa uma classe para importar sons e tocar a musica
 from audios import Audios
 
-#importa uma classe para cirar textos na tela
+# Importa uma classe para cirar textos na tela
 from textos import Texto
+
+def main_menu():   
+    menu_loop = True
+    clicked = False
+    while menu_loop:
+        menu_display.fill(BACKGROUND_COLOR)
+        drawGroup.draw(menu_display)
+
+        Texto("MAIN MENU", 30, (WHITE_PY), 160, 200, menu_display)
+
+        mx, my = pygame.mouse.get_pos()
+
+        # Botões
+        start_game_btn = pygame.Rect(160, 295, 270, 40)
+        pygame.draw.rect(menu_display, (BACKGROUND_COLOR), start_game_btn)
+        Texto("START GAME", 25, (PYTHON_BLUE), 170, 300, menu_display)
+
+        options_btn = pygame.Rect(160, 345, 270, 40)
+        pygame.draw.rect(menu_display, (BACKGROUND_COLOR), options_btn)
+        Texto("OPTIONS", 25, (PYTHON_BLUE), 215, 350, menu_display)
+
+        credits_btn = pygame.Rect(160, 395, 270, 40)
+        pygame.draw.rect(menu_display, (BACKGROUND_COLOR), credits_btn)
+        Texto("CREDITS", 25, (PYTHON_BLUE), 215, 400, menu_display)
+
+        if start_game_btn.collidepoint((mx, my)):
+            if clicked:
+                break
+
+
+
+        for event in pygame.event.get():
+            # Sai do MENU
+            if event.type == pygame.QUIT:
+                menu_loop = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    menu_loop = False
+                elif event.key == pygame.K_SPACE:
+                    audios.certeza.play()
+
+            if start_game_btn.collidepoint((mx, my)):
+                if event.type == MOUSEBUTTONDOWN:
+                    print("chegou aqui")
+                    menu_loop = False
+
+        pygame.display.update()
 
 pygame.init()
 Gameloop = True
 Playing = False
 
-#inicializa os audios do jogo
+# CORES
+BACKGROUND_COLOR = (68, 73, 80)
+PYTHON_BLUE = (30, 144, 255)
+WHITE_PY = (255, 255, 255)
+
+# Inicializa os audios do jogo
 audios = Audios()
 
-#configura a janela
+# Configura a janela
 SCREEN_SIZE = (600, 695)
 display = pygame.display.set_mode(SCREEN_SIZE)
+menu_display = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption('PyMillion')
 
-#define sprites
+# Define sprites
 drawGroup = pygame.sprite.Group()
 python = pygame.sprite.Sprite(drawGroup)
 python.image = pygame.image.load('src/img/PythonImg.png')
 python.image = pygame.transform.scale(python.image, [100, 100])
 python.rect = pygame.Rect(455, 40, 100, 100)
 
+main_menu()
 
 if __name__ == "__main__":
     while Gameloop:
 
-        #define objetos da janela
-        display.fill([68, 73, 80])
+        
+        # Define objetos da janela
+        display.fill(BACKGROUND_COLOR)
         drawGroup.draw(display)
 
         Texto('Py',30,(30,144,255), 275, 40, display)
         Texto('Million',30,(251, 236, 93), 205, 90, display)
 
         for event in pygame.event.get():
-            #fecha a janela
+            # Fecha a janela
             if event.type == pygame.QUIT:
                 Gameloop = False
             elif event.type == pygame.KEYDOWN:
@@ -47,7 +105,7 @@ if __name__ == "__main__":
                     audios.certeza.play()
 
 
-        #temporariamente move a logo do python
+        # Temporariamente move a logo do python
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
             python.rect.x -= 1

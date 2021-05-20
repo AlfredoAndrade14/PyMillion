b93bf561b40ee376ffddd434091c7be7ec7b1d1f
Andre Alves <andre.alves@ccc.ufcg.edu.br>
2021-05-20 09:53:34 -0300

    Adicionado o easter egg do t e a opção de quit

--- menu.py
+++ menu.py
@@ -1,6 +1,6 @@
 import pygame
 
-from pygame.constants import MOUSEBUTTONDOWN
+from pygame.constants import KEYDOWN, K_t, MOUSEBUTTONDOWN
 
 from textos import Texto
 
@@ -29,25 +29,29 @@ class Menu():
             
             objectGroup.draw(menu_display)
 
-            Texto('Py',30,(30,144,255), 275, 40, menu_display,"8-Bit")
-            Texto('Million',30,(251, 236, 93), 205, 90, menu_display,"8-Bit")
+            Texto('Py',30,(30,144,255), 270, 40, menu_display,"8-Bit")
+            Texto('Million',30,(251, 236, 93), 200, 90, menu_display,"8-Bit")
 
             Texto("MAIN MENU", 30, (WHITE_PY), 160, 200, menu_display, "Pixel")
 
             mx, my = pygame.mouse.get_pos()
 
             # Botões
-            start_game_btn = pygame.Rect(160, 295, 270, 40)
+            start_game_btn = pygame.Rect(170, 295, 245, 40)
             pygame.draw.rect(menu_display, (BACKGROUND_COLOR), start_game_btn)
             Texto("START GAME", 25, (PYTHON_BLUE), 170, 300, menu_display, "Pixel")
 
-            options_btn = pygame.Rect(160, 345, 270, 40)
+            options_btn = pygame.Rect(200, 345, 185, 40)
             pygame.draw.rect(menu_display, (BACKGROUND_COLOR), options_btn)
-            Texto("OPTIONS", 25, (PYTHON_BLUE), 215, 350, menu_display, "Pixel")
+            Texto("OPTIONS", 25, (PYTHON_BLUE), 208, 350, menu_display, "Pixel")
 
-            credits_btn = pygame.Rect(160, 395, 270, 40)
+            credits_btn = pygame.Rect(200, 395, 185, 40)
             pygame.draw.rect(menu_display, (BACKGROUND_COLOR), credits_btn)
-            Texto("CREDITS", 25, (PYTHON_BLUE), 215, 400, menu_display, "Pixel")
+            Texto("CREDITS", 25, (PYTHON_BLUE), 208, 400, menu_display, "Pixel")
+
+            quit_btn = pygame.Rect(240, 445, 105, 40)
+            pygame.draw.rect(menu_display, (BACKGROUND_COLOR), quit_btn)
+            Texto("QUIT", 25, (PYTHON_BLUE), 245, 450, menu_display, "Pixel")
 
             for event in pygame.event.get():
                 # Sai do MENU
@@ -59,9 +63,23 @@ class Menu():
                         Gameloop = False
                         return Gameloop
 
+                # Inicia o jogo
                 if start_game_btn.collidepoint((mx, my)):
                     if event.type == MOUSEBUTTONDOWN:
                         Gameloop = True
                         return Gameloop
+                # Sai do jogo
+                elif quit_btn.collidepoint((mx, my)):
+                    if event.type == MOUSEBUTTONDOWN:
+                        pygame.quit()
+                        quit()
+
+                if event.type == KEYDOWN:
+                    if event.key == K_t:
+                        objectGroup.empty()
+                elif event.type == pygame.KEYUP:
+                    if event.key == K_t:
+                        objectGroup.add(python)
+                    
 
             pygame.display.update()
